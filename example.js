491c0cf05229757a9cb693638fb7bd8697be7891
Karanpreet Singh <singhkaranpreet710@gmail.com>
2022-01-18 18:43:51 +0530

    knock knock code added in example.js

--- example.js
+++ example.js
@@ -1,5 +1,7 @@
 function greet(name)
 {
-    console.log("Hello " + name);
+    console.log("Knock Knock " + name);
+    console.log("Knock Knock " + name);
+    console.log("Knock Knock " + name);
 }
 greet("Karan")
